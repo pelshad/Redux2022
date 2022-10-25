8b6b9eade643e6f859b4f68a9158d72c911b05bb
pelshad <akuaru@gmail.com>
2022-10-25 10:47:11 +0900

    로그아웃 구현

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 // Action을 사용하기 위해 import
-import { login } from '../redux/user';
+import { login, logout } from '../redux/user';
 // user의 reducers의 login 메소드 사용
 
 const Login = () => {
@@ -12,6 +12,9 @@ const Login = () => {
             <button onClick={() => {
                 dispatch(login({name: "내 이름", age: 20, email:"test@gmail.com"}))
             }}>Login</button>
+            <button onClick={() => {
+                dispatch(logout())
+            }}>Logout</button>
         </div>
     );
 
--- src/redux/user copy.js
+++ src/redux/user copy.js
@@ -1,28 +1,21 @@
 //user.js 유저 정보를 담는 Reducer
-import { createSlice } from '@reduxjs/toolkit';
 
-const initialStateValue = {name: "", age: 0, email:""} // 값 세팅
+import { createSlice } from '@reduxjs/toolkit';
 
 // Reducer 생성
 // crateSlice -> 기존에 createReducer와 createAction을 동시에 해줌
 export const userSlice = createSlice({
     name: "user", // Reducer 이름
-    initialState: { value: initialStateValue},
+    initialState: { value: {name: "", age: 0, email:""}}, // value 및 초기값 세팅
     reducers:{ // 상태가 변할 경우 실행될 것
-        // 로그인
         login: (state, action) => {
             state.value = action.payload
         },
-        //로그아웃
-        logout: (state) => {
-            state.value = initialStateValue
-        }
-        // 다른 login메소드를 실행하였 을 때 작동 
-
+        //login메소드를 실행하였 을 때 작동 
     },
 });
 // 생성된 Reducer의 값은 store.js에 import하여 관리
 
-export const { login, logout } = userSlice.actions;
+export const { login } = userSlice.actions;
 // 위에 만들어둔 login 메소드를 action기능이 작동하도록 다른데서 씀
 export default userSlice.reducer;
